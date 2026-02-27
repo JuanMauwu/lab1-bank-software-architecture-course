c57b856c5c8d3166d31b7db66e84b60326cdd2a0
JuanMauwu <juanmavc111@gmail.com>
2026-02-27 18:19:30 -0500

    Initial commit

---
+++ src/main/java/com/udea/lab1banco/Lab1bancoApplication.java
@@ -0,0 +1,13 @@
+package com.udea.lab1banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1bancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1bancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udea/lab1banco/Lab1bancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.lab1banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1bancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
